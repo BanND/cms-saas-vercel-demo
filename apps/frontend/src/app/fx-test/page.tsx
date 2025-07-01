20608cb41f1d8d5d7f77e69fd8c5acfc6ffc7968
toan.tran <toan.tran@optimizely.com>
2025-07-01 14:02:50 +0700

    @optimizely/react-sdk starter kit

---
+++ apps/frontend/src/app/fx-test/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { OptimizelyConfig } from "@optimizely/optimizely-sdk";
+import {
+  OptimizelyProvider,
+  ReactSDKClient,
+  createInstance,
+} from "@optimizely/react-sdk";
+import { useEffect, useState } from "react";
+import { Decision } from "@components/fx-test/decision";
+import FlagsOffMessage from "@components/fx-test/flags-off-message";
+import { Pre } from "@components/fx-test/pre";
+
+const optimizelyClient: ReactSDKClient = createInstance({
+  sdkKey: "HZ88m4YEBFMS8H36KdKNV",
+});
+
+const userIds: string[] = [];
+while (userIds.length < 10) {
+  // to get rapid demo results, generate an array of random users. Each user always sees the same variation unless you reconfigure the flag rule.
+  userIds.push((Math.floor(Math.random() * 999999) + 100000).toString());
+}
+
+function Page() {
+  const [hasOnFlag, setHasOnFlag] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  const [isClientReady, setIsClientReady] = useState<Boolean>(false);
+  const [projectId, setProjectId] = useState<string>();
+
+  useEffect(() => {
+    optimizelyClient.onReady().then(() => {
+      console.log("Optimizely Client is ready");
+      setIsDone(true);
+      if (isClientValid()) {
+        setIsClientReady(true);
+        setProjectID(optimizelyClient.getOptimizelyConfig());
+      }
+    }).catch((err) => {
+      console.error("Error initializing Optimizely Client:", err);
+      // Handle error
+  });
+  }, []);
+
+  const isClientValid = (): boolean => {
+    const optimizelyConfig = optimizelyClient.getOptimizelyConfig();
+    console.log("Optimizely Config:", optimizelyConfig);
+    return optimizelyConfig !== null;
+  };
+
+  const setProjectID = (optimizelyConfig: OptimizelyConfig | null): void => {
+    if (!optimizelyConfig) return;
+    const datafile = JSON.parse(optimizelyConfig.getDatafile());
+    setProjectId(datafile.projectId);
+  };
+
+  return (
+    <OptimizelyProvider
+      optimizely={optimizelyClient}
+      // Generally React SDK runs for one client at a time i.e for one user throughout the lifecycle.
+      // You can provide the user Id here once and the SDK will memoize and reuse it throughout the application lifecycle.
+      // For this example, we are simulating 10 different users so we will ignore this and pass override User IDs to the useDecision hook for demonstration purpose.
+      user={{ id: "default_user" }}
+    >
+      <pre>Welcome to our Quickstart Guide!</pre>
+      {isClientReady && (
+        <>
+          {userIds.map((userId) => (
+            <Decision
+              key={userId}
+              userId={userId}
+              setHasOnFlag={setHasOnFlag}
+            />
+          ))}
+          <br />
+          {!hasOnFlag && projectId && <FlagsOffMessage projectId={projectId} />}
+        </>
+      )}
+      {isDone && !isClientReady && (
+        <Pre>
+          Optimizely client invalid. Verify in Settings -{">"} Environments that
+          you used the primary environment&lsquo;s SDK key
+        </Pre>
+      )}
+    </OptimizelyProvider>
+  );
+}
+
+export default Page;
